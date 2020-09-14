bb204683d29ca3411b61565650ce5d3727d75241
David Wright <davidtwright89@gmail.com>
2020-09-14 00:52:38 -0400

    Implemented finder() in ex5.py

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,21 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    paths_for_file = {}
+    for path in files:
+        file_name_start_index = path.rfind('/') + 1
+        file_name = path[file_name_start_index:]
+        if file_name in paths_for_file:
+            paths_for_file[file_name] += [path]
+        else:
+            paths_for_file[file_name] = [path]
+    
+    paths_queried = {}
+    for query in queries:
+        if query in paths_for_file:
+            paths = paths_for_file[query]
+            for path in paths:
+                paths_queried[path] = path
 
-    return result
+    return list(paths_queried.keys())
 
 
 if __name__ == "__main__":
