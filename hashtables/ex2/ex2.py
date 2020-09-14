61a9394767afd955de543b81f82820666f72c244
David Wright <davidtwright89@gmail.com>
2020-09-13 22:23:34 -0400

    Implemented reconstruct_trip() in ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,32 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    destination_dict = {}
+    for ticket in tickets:
+        destination_dict[ticket.source] = ticket.destination
+    
+    route = [None] * length
+    route[0] = destination_dict["NONE"]
+    for i in range(1, length):
+        route[i] = destination_dict[route[i - 1]]
 
     return route
+
+
+# tickets = [
+#     Ticket("PIT", "ORD"),
+#     Ticket("XNA", "CID"),
+#     Ticket("SFO", "BHM"),
+#     Ticket("FLG", "XNA"),
+#     Ticket("NONE", "LAX"),
+#     Ticket("LAX", "SFO"),
+#     Ticket("CID", "SLC"),
+#     Ticket("ORD", "NONE"),
+#     Ticket("SLC", "PIT"),
+#     Ticket("BHM", "FLG")
+# ]
+# length = 10
+# expected_output = ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD", "NONE"]
+# output = reconstruct_trip(tickets, length)
+
+# print(f"Expected Output: {expected_output}\nActual Output:   {output}")
