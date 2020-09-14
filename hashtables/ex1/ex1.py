428118c2d7306ddfaa9ff3ff02d7f87d609c88be
David Wright <davidtwright89@gmail.com>
2020-09-13 21:53:59 -0400

    Implemented get_indices_of_item_weights() in ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    weight_dict = {}
+    for (index, weight) in enumerate(weights):
+        limit_minus_weight = limit - weight
+        if limit_minus_weight in weight_dict:
+            return (index, weight_dict[limit_minus_weight])
+        weight_dict[weight] = index
+    
     return None
+
+# limit = 21
+# weights = [ 4, 6, 10, 15, 16]
+# length = 5
+# expected_output = (3, 1)
+
+# output = get_indices_of_item_weights(weights, length, limit)
+
+# print(f"expected output: {expected_output}, output: {output}")
